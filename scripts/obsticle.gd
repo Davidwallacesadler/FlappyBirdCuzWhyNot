40affc182565f03ad75ba84c8a1ae75f7d197cf2
David Sadler <davidwallacesadler@gmail.com>
2023-11-16 20:50:32 -0700

    Get obsticles with gaps working right

--- scripts/obsticle.gd
+++ scripts/obsticle.gd
@@ -1,32 +1,45 @@
 extends Node2D
 
-@export var window_size: float = 50
+@export var gap_size: float = 50
 @export var max_height: float = 200
 
+@export_category("Internal Nodes")
+
+@export var top_area: Area2D
+@export var bottom_area: Area2D
+@export var top_collision_shape: CollisionShape2D
+@export var bottom_collision_shape: CollisionShape2D
+
 func _ready():
-	var window_start_position = _get_window_start()
-	print("Window start", window_start_position)
-	_setup_top_obsticle(window_start_position)
-	_setup_bottom_obsticle(max_height - (window_start_position + window_size))
+	var gap_start_position = _get_gap_start()
+	_setup_top_obsticle(gap_start_position)
+	
+	var gap_end_position = gap_start_position + gap_size
+	var bottom_obsticle_height = max_height - gap_end_position
+	_setup_bottom_obsticle(bottom_obsticle_height)
 	
 
-func _get_window_start() -> float:
+func _get_gap_start() -> float:
 	var start = 0.0
-	var end = max_height - window_size
+	var end = max_height - gap_size
 	var rng = RandomNumberGenerator.new()
 	return rng.randf_range(start, end)
 	
 
 func _setup_top_obsticle(height: float):
-	$TopArea/CollisionShape2D.shape.size.y = height
+	top_collision_shape.shape = _create_new_shape(height)
 	var y_offset_position = height * 0.5
-	$TopArea.position = Vector2(0, y_offset_position)
-	print("Top position: ", $TopArea.position, "Height: ", height)
+	top_area.position = Vector2(0, y_offset_position)
 	
 
 func _setup_bottom_obsticle(height: float):
-	$BottomArea/CollisionShape2D.shape.size.y = height
+	bottom_collision_shape.shape = _create_new_shape(height)
 	var y_offset_position = max_height - (height * 0.5)
-	$BottomArea.position = Vector2(0, y_offset_position)
-	print("Bottom position: ", $BottomArea.position, "Height: ", height)
+	bottom_area.position = Vector2(0, y_offset_position)
 	
+
+func _create_new_shape(height: float) -> RectangleShape2D:
+	var new_shape = RectangleShape2D.new()
+	new_shape.size.x = 20
+	new_shape.size.y = height
+	return new_shape
