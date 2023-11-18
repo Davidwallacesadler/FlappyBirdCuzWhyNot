5f87316c4af9b24856b996c5293e857393899ded
David Sadler <davidwallacesadler@gmail.com>
2023-11-17 21:23:01 -0700

    Add placeholder obsticle sprites

--- scripts/obsticle.gd
+++ scripts/obsticle.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
-# TODO: Make this a range
-@export var gap_size: float = 250
+@export var gap_min_size: float = 200
+@export var gap_max_size: float = 350
+
 @export var max_height: float = 648
 @export var width: float = 100
 @export var speed: float = 100
@@ -16,12 +17,10 @@ extends Node2D
 @export var bottom_collision_shape: CollisionShape2D
 @export var score_collision_shape: CollisionShape2D
 
-# JUST FOR TESTING
-var _initial_position: Vector2
+@export var top_sprite: Sprite2D
+@export var bottom_sprite: Sprite2D
 
 func _ready():
-	_initial_position = self.position
-	
 	top_area.body_entered.connect(_on_body_entered_obsticle)
 	top_area.area_entered.connect(_on_area_entered_obsticle)
 	bottom_area.body_entered.connect(_on_body_entered_obsticle)
@@ -38,16 +37,24 @@ func _process(delta):
 	
 
 func _setup_new_obsticle_gap() -> void:
-	var gap_start_position = _get_gap_start_position()
+	var gap_size = _get_random_gap_size()
+	var gap_start_position = _get_gap_start_position(gap_size)
+	
 	_setup_top_obsticle(gap_start_position)
 	
 	var gap_end_position = gap_start_position + gap_size
 	var bottom_obsticle_height = max_height - gap_end_position
-	_setup_bottom_obsticle(bottom_obsticle_height)
 	
+	_setup_bottom_obsticle(bottom_obsticle_height)
 	_setup_score_area(gap_size, gap_start_position)
+	
 
-func _get_gap_start_position() -> float:
+func _get_random_gap_size() -> float:
+	var rng = RandomNumberGenerator.new()
+	return rng.randf_range(gap_min_size, gap_max_size)
+	
+
+func _get_gap_start_position(gap_size: float) -> float:
 	var start = 0.0
 	var end = max_height - gap_size
 	var rng = RandomNumberGenerator.new()
@@ -56,19 +63,41 @@ func _get_gap_start_position() -> float:
 
 func _setup_top_obsticle(height: float) -> void:
 	top_collision_shape.shape.size.y = height
+	
 	var y_offset_position = height * 0.5
 	top_area.position = Vector2(0, y_offset_position)
 	
+	var image_rect = top_sprite.get_rect()
+	var x_scale_factor = width / image_rect.size.x 
+	var y_scale_factor = height / image_rect.size.y
+	
+	top_sprite.scale.x = x_scale_factor
+	top_sprite.scale.y = y_scale_factor
+	
+	var sprite_offset = image_rect.size.y * y_scale_factor * 0.5
+	top_sprite.position = Vector2(0, sprite_offset)
 
 func _setup_bottom_obsticle(height: float) -> void:
 	bottom_collision_shape.shape.size.y = height
 	var y_offset_position = max_height - (height * 0.5)
 	bottom_area.position = Vector2(0, y_offset_position)
 	
+	var image_rect = bottom_sprite.get_rect()
+	var x_scale_factor = width / image_rect.size.x 
+	var y_scale_factor = height / image_rect.size.y
+	
+	bottom_sprite.scale.x = x_scale_factor
+	bottom_sprite.scale.y = y_scale_factor
+	
+	var sprite_offset = image_rect.size.y * y_scale_factor * 0.5
+	bottom_sprite.position = Vector2(0, max_height - sprite_offset)
+	
 
 func _setup_score_area(height: float, y_offset: float) -> void:
 	score_collision_shape.shape.size.y = height
-	score_area.position = Vector2(width, y_offset + (height * 0.5))
+	score_collision_shape.shape.size.x = 20
+	score_area.position = Vector2(0, y_offset + (height * 0.5))
+	
 
 func _create_new_shape() -> RectangleShape2D:
 	var new_shape = RectangleShape2D.new()
@@ -80,7 +109,7 @@ func _on_body_entered_obsticle(body: Node2D):
 	GameEvents.player_touched_obsticle.emit()
 
 func _on_area_entered_obsticle(area: Area2D):
-	self.position = _initial_position
+	self.position = Vector2.ZERO
 	_setup_new_obsticle_gap.call_deferred()
 
 func _on_body_entered_score(body: Node2D):
--- scripts/obsticle_spawner.gd
+++ scripts/obsticle_spawner.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @export var spawn_time: float = 3.0
-@export var count: int = 5
+@export var count: int = 4
 
 var _obsticle_scene = preload("res://scenes/obsticle.tscn")
 
@@ -13,9 +13,14 @@ func _ready():
 	timer.wait_time = spawn_time
 	timer.timeout.connect(_on_spawn_timer_finished)
 	add_child(timer)
+	_add_obsticle()
 
 func _on_spawn_timer_finished() -> void:
-	add_child(_obsticle_scene.instantiate())
-	count -= 1
+	_add_obsticle()
 	if (count == 0):
 		timer.stop()
+
+func _add_obsticle() -> void:
+	add_child(_obsticle_scene.instantiate())
+	count -= 1
+	
